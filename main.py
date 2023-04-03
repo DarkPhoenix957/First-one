5c8e0f34a9553c584690458c8d247dd04bc67eb8
DarkPhoenix957 <easy_d@list.ru>
2023-04-03 15:30:09 +0300

    Площадь и перимерт треугольника

---
+++ main.py
@@ -0,0 +1,16 @@
+def triangles():
+    b = int(input())
+    h = int(input())
+    s = (b*h)/2
+    print(s)
+
+
+def trianglep():
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    p = a+b+c
+    print(p)
+
+
+
