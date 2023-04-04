cb40fc35c0c6ce6332bb53c17382dc2005aea854
DarkPhoenix957 <easy_d@list.ru>
2023-04-04 13:53:34 +0300

    Конвертер долларов в рубли или евро

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import sys
 def converter():
     req1 = input("Введите евро или рубли ")
+    if req1 != "рубли" or "евро":
+        print("Пожалуйста, следуйте инструкции")
+        sys.exit()
     req2 = int(input("Введите количество долларов "))
     ed = req2*0.916
     rd = req2*77.95
     if req1 == "евро":
         print(ed)
-    else:
+    elif req1 == "рубли":
         print(rd)
 
 
-
+converter()
 
